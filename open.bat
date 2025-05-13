fa4737eb36083ec022b244b924e4ba1d083bfde8
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2025-05-13 19:27:38 +0500

    page

---
+++ open.bat
@@ -0,0 +1,6 @@
+@echo off
+@REM REM Start backend
+@REM pm2 start server.js --name my-backend
+
+REM Open installed PWA
+start chrome --profile-directory=Default --app-id=lobmifadjcajdicnlfkojaoichaidnlb
